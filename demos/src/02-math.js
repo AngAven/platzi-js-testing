2131cd62e5bce6d3960d6df3294fdadee1c17dda
Angel Avendaño Cruz <angel.aven.c@gmail.com>
2025-02-04 02:03:25 -0600

    Eslint configuration
    - Search and fix

--- demos/src/02-math.js
+++ demos/src/02-math.js
@@ -7,10 +7,10 @@ function multiply(a, b) {
 }
 
 function divide(a, b) {
-  if (b === 0) return null
+  if (b === 0) return null;
   return a / b;
 }
 
 module.exports = {
-  sum, multiply, divide
+  sum, multiply, divide,
 };
--- demos/src/02-math.test.js
+++ demos/src/02-math.test.js
@@ -1,21 +1,21 @@
-const {sum, multiply, divide} = require('./02-math')
+const { sum, multiply, divide } = require('./02-math');
 
-test("adds 1 + 3 should be 4", () => {
-  const answer = sum(1, 4)
-  expect(answer).toBe(5)
-})
+test('adds 1 + 3 should be 4', () => {
+  const answer = sum(1, 4);
+  expect(answer).toBe(5);
+});
 
-test("multiply 2 * 2 should be 4)", () => {
-  const answer = multiply(2, 2)
-  expect(answer).toBe(4)
-})
+test('multiply 2 * 2 should be 4)', () => {
+  const answer = multiply(2, 2);
+  expect(answer).toBe(4);
+});
 
-test("divide 10 / 2 should be 5", () => {
-  const answer = divide(10, 2)
-  expect(answer).toBe(5)
-})
+test('divide 10 / 2 should be 5', () => {
+  const answer = divide(10, 2);
+  expect(answer).toBe(5);
+});
 
-test("divide by zero should be null", () => {
-  const answer = divide(100, 0)
-  expect(answer).toBeNull()
-})
+test('divide by zero should be null', () => {
+  const answer = divide(100, 0);
+  expect(answer).toBeNull();
+});
