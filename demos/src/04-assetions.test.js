20c782a94d9fb6d9f81cd37d523b52d6d202ed24
Angel Avendaño Cruz <angel.aven.c@gmail.com>
2025-02-04 02:28:51 -0600

    Using matchers | assertions

--- demos/src/04-assetions.test.js
+++ demos/src/04-assetions.test.js
@@ -1,5 +1,30 @@
-test('test obj', () => {
-  const data  = { name: 'angel'}
+test('obj', () => {
+  const data = { name: 'angel'}
   data.lastName = 'avendaño'
   expect(data).toEqual({name: 'angel', lastName: 'avendaño'})
+})
+
+test('null', () => {
+  const data = null
+  expect(data).toBeNull()
+  expect(data).toBeDefined()
+  expect(data).not.toBeUndefined()
+})
+
+test('booleans', () => {
+  expect(true).toEqual(true)
+  expect(false).toEqual(false)
+
+  expect(0).toBeFalsy()
+  expect(false).toBeFalsy()
+})
+
+test('strings', () => {
+  expect('Angel Avendañostop').toMatch(/stop/)
+})
+
+
+test('list | array', () => {
+  const numbers = [1, 2, 3, 4, 5]
+  expect(numbers).toContain(3)
 })
